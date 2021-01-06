9acc301921a869010f50156c7c214d06ffe1c077
kamssa <kamssa0@gmail.com>
2021-01-06 01:42:42 +0000

    modif

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/dashboardImmobilier'));
+app.use(express.static('./dist/gstoreplus'));
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/dashboardImmobilier/'}),
+  res.sendFile('index.html', {root: 'dist/gstoreplus/'}),
 );
 
 app.listen(process.env.PORT || 8080);
